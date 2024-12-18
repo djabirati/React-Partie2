c47c9df7042c8f9da76171a90a57f97d8ad2e109
Djabirati Marthadi <djabirati@MacBook-Pro-de-Djabirati.local>
2024-12-18 14:53:00 +0100

    Gestion etat avec booleen

--- src/components/containers/Menu/Menu.jsx
+++ src/components/containers/Menu/Menu.jsx
@@ -3,15 +3,17 @@ import {Bouton} from "../../ui/Button/Button.jsx";
 
 export function Menu() {
 
-    const [textButton, changeText] = useState('Show')
+    const [isTextVisible, changeText] = useState(false)
     function changeTextButton() {
-        changeText(textButton === 'Show' ? 'Hide' : 'Show');
+        changeText(!isTextVisible);
     }
 
     return (
         <>
-            <Bouton gereClique={changeTextButton}>{textButton}</Bouton>
-            <div className={textButton === 'Show' ? 'hidden' : 'visible'}>
+            <Bouton gereClique={changeTextButton}>
+                {isTextVisible ? 'Hide' : 'Show'}
+            </Bouton>
+            <div className={isTextVisible ? 'block mt-4 p-4 bg-gray-100 rounded shadow' : 'hidden'}>
                 <h2>Menu Secret</h2>
                 <ul>
                     <li>Nourrir le chien</li>
@@ -21,6 +23,4 @@ export function Menu() {
             </div>
         </>
     )
-
-
 }
